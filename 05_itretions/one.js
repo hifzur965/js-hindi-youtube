0e458059fb7c4d40639aa5ad268b4dca486d1ef3
hifzur965 <h.rahman1989@gmail.com>
2025-01-24 07:14:38 +0000

    comit one

---
+++ 05_itretions/one.js
@@ -0,0 +1,22 @@
+//fro
+
+for (let i = 1; i <= 10; i++) {
+    const element = i;
+    if(element==5){
+        // console.log("5 is best no");
+    }
+    // console.log(element);
+}
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`outer loop: ${i}`);
+    
+    for (let j = 1; j <=10; j++) {
+        // console.log(`inner loop: ${j} and inner loop ${i}`);
+       // console.log(i+ "*" + j + "=" + i*j);
+        console.log(`table From ${i} to ${j} :${i}*${j} = ${i*j}`);          
+        
+    }
+    
+}
